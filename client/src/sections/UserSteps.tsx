12570e1cc7cb68a1ae19bfe54e344e18e657b7fc
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2025-03-29 02:31:44 -0700

    linting issue

--- client/src/sections/UserSteps.tsx
+++ client/src/sections/UserSteps.tsx
@@ -25,7 +25,7 @@ export default function UserSteps() {
     <div className="py-4 flex flex-col items-center">
       <h3 className="text-2xl pb-2 font-bold">Steps</h3>
       <div className="flex flex-col items-center gap-2 w-full md:w-auto">
-        {getUserStepsWithEmpty().map((step, index) => (
+        {getUserStepsWithEmpty().map((_, index) => (
           <div key={`step-${index}-container`} className="flex flex-row gap-2 items-center w-full md:w-lg">
             <p key={`step-${index}-number`} className="w-6">{index + 1}.</p>
             <input 
