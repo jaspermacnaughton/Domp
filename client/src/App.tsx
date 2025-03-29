7e6db3ce4fc2721231c91afd132e6f5efb3df18f
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2025-03-29 00:59:40 -0700

    Basic formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import { MathJax, MathJaxContext } from 'better-react-mathjax'
+import "./App.css"
+import { useState, useEffect } from "react"
+import { MathJax, MathJaxContext } from "better-react-mathjax"
 
 function App() {
-  const [problem, setProblem] = useState<string>('');
+  const [title, setTitle] = useState<string>("");
+  const [problem, setProblem] = useState<string>("");
+  const [answer, setAnswer] = useState<string>("");
   
   const fetchProblem = async () => {
-    console.log('Fetching problem...');
-    const response = await fetch('http://localhost:8080/api/problem');
+    setProblem("");
+    
+    const response = await fetch("http://localhost:8080/api/problem");
+    if (!response.ok) {
+      console.error(`Failed to fetch problem: ${response.statusText}`);
+      return;
+    }
+    
     const data = await response.json();
+    setTitle(data.title);
     setProblem(data.problem);
   }
 
@@ -18,9 +27,24 @@ function App() {
 
   return (
     <MathJaxContext>
-      <h2 className="text-3xl">Daily Online Math Problem</h2>
+      <h2 className="text-3xl pb-4">Daily Online Math Problem</h2>
       
+      <hr className="my-4" />
+      
+      <h3 className="text-2xl pb-2 font-bold">Problem</h3>
+      <h4 className="text-xl pb-2">{title}</h4>
       <MathJax className="text-1xl">{problem}</MathJax>
+      
+      <hr className="my-4 mt-6" />
+      
+      <div className="py-4"> 
+        <h3 className="text-2xl pb-2 font-bold">Answer</h3>
+        <input
+          className="border border-gray-300 rounded-md pl-2 p-1 w-full md:w-lg" 
+          placeholder="Enter your answer"
+          onBlur={(e) => setAnswer(e.target.value)}
+        />
+      </div>
     </MathJaxContext>
   )
 }
--- server/main.py
+++ server/main.py
@@ -4,13 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, origins="*")
 
-@app.route('/')
-def index():
-    return "Hello, World!"
-
 @app.route('/api/problem', methods=['GET'])
 def get_problem():
-    return {"problem": "$$ 2x + 3y = 10 $$\n$$ 5x - 2y = 4 $$"}
+    return {
+        "title": "Solve the system of equations:",
+        "problem": "$$ 2x + 3y = 10 $$\n$$ 5x - 2y = 4 $$"
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
