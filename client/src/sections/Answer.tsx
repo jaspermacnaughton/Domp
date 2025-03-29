0d083abef4badcc8deb772cc753369af1e716a5e
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2025-03-29 02:54:51 -0700

    Add MathQuill for answer for better typing

--- client/src/sections/Answer.tsx
+++ client/src/sections/Answer.tsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
+import { addStyles, EditableMathField, MathField } from "react-mathquill";
 
+// Initialize MathQuill
+addStyles();
 
 export default function Answer() {
-  const [, setAnswer] = useState<string>("");
-  
+  const [latex, setLatex] = useState<string>("");
+
+  const handleChange = (mathField: MathField) => {
+    setLatex(mathField.latex());
+  };
+
   return (
     <div className="py-4"> 
-        <h3 className="text-2xl pb-2 font-bold">Answer</h3>
-        <input
-          className="border border-gray-300 rounded-md pl-2 p-1 w-full md:w-lg" 
-          placeholder="Enter your answer"
-          onBlur={(e) => setAnswer(e.target.value)}
-        />
-      </div>
+      <h3 className="text-2xl pb-2 font-bold">Answer</h3>
+      <EditableMathField
+      className="border border-gray-300 rounded-md pl-2 p-1 w-full md:w-lg"
+        latex={latex}
+        onChange={handleChange}
+      />
+    </div>
   );
 }
